6b61ffd01fc76a956eab63093b56ad5933616d2f
Modulus <thejohnskauge@gmail.com>
2015-03-11 15:49:09 +0100

    First commit

---
+++ src/main/java/com/rubblesnask/configuration/DemoConfiguration.java
@@ -0,0 +1,10 @@
+package com.rubblesnask.configuration;
+
+import io.dropwizard.Configuration;
+
+/**
+ * Created by Modulus on 11.03.2015.
+ */
+public class DemoConfiguration extends Configuration {
+
+}
---
+++ src/main/java/com/subblesnask/types/Wail.java
@@ -0,0 +1,49 @@
+package com.subblesnask.types;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.validation.MaxSize;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+/**
+ * Created by Modulus on 11.03.2015.
+ */
+public class Wail {
+
+    public Wail() {
+        date = LocalDate.now();
+    }
+
+    public Wail(String message){
+        this.message = message;
+        this.date = LocalDate.now();
+    }
+
+    @NotNull
+    private LocalDate date;
+
+    @NotNull
+    @NotEmpty
+    @MaxSize(value = 255)
+    private String message;
+
+    @JsonProperty
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @JsonProperty
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
